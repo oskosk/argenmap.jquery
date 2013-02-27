657ee0cdfa129b13d89182596908d2ddcfd576e7
igm <igm@webserver.ign.gob.ar>
2013-02-27 15:39:46 -0300

    agregué logo ign sin texto

--- docs/cargar.php
+++ docs/cargar.php
@@ -2,6 +2,8 @@
 $casos = array(
 	'http://www.argentina.gob.ar/directorio/mapa/' =>
 		array('solucion'=>'1.html'),
+	'http://www.desarrollosocial.gov.ar/Mapa.aspx' =>
+		array('solucion'=>'3.html'),
 	'http://malvinasonline.com.ar/' =>
 		array('solucion'=>'2.html')	
 );
@@ -9,6 +11,12 @@ $caso = $_GET['caso'];
 //chequear if in array
 
 //chequear if file_exists
+echo $caso;
+print_r( array_keys($casos) );
+if (! array_key_exists($caso, $casos ) ) {
+	die('1');
+}
+
 $pagina = file_get_contents( $caso );
 
 $solucion = file_get_contents('casos/'. $casos[$caso]['solucion']);
