c431a51761dea8a3fb5fae63a031efbe21fbca91
Christian Gastrell <cg@ubuntu-vm.(none)>
2014-01-10 16:35:58 -0300

    agrega la funcionalidad de getFeatureInfo

--- argenmap.jquery.js
+++ argenmap.jquery.js
@@ -89,6 +89,8 @@
     this.opts = $.extend({}, _defaults, opciones);
     this.gmap = null;
     this._marcadores = {};
+    this.capasWms = [];
+    this.infoMenuActivado = false;
 
     //-----------------------------------------------------------------------//
     // funciones de Argenmap
@@ -129,9 +131,116 @@
       argenmap.GmapAgregarCapaBase(map, new argenmap.CapaBaseArgenmap());
       argenmap.GmapAgregarCapa(map, new argenmap.CapaTMSArgenmap());
       this.gmap.setMapTypeId('Mapa IGN');
+
       return true;
     };
+    this.activarInfoMenu = function() {
+      //infoMenu
+      if(this.infoMenuActivado === true) {
+        return;
+      }
+      this.infoMenuActivado = true;
+      google.maps.event.addListener(map, 'click', $.proxy(this.mostrarInfoMenu,this));
+      $( '.argenmapMapCanvas' ).on( "mouseenter" ,"a.argenmapFeatureInfo", function(e) {
+        $(this).css({
+          'background-color':"#1C74A5",
+          color:'white'
+        });
+      });
+      $( '.argenmapMapCanvas' ).on( "mouseleave" ,"a.argenmapFeatureInfo", function(e) {
+        $(this).css({
+          'background-color':"transparent",
+          color: '#1C74A5'
+        });
+      });
+      $( '.argenmapMapCanvas' ).on( "click", "a.argenmapFeatureInfo", $.proxy(this.pedirInformacion,this));
+    };
+    this.desactivarInfoMenu = function() {
+      google.maps.event.clearListeners(map, 'click');
+    };
+    this.mostrarInfoMenu = function(event) {
+      var infow = this.infoWindow();
+      infow.close();
+      var c = '<div id="infoMenu" style="width:200px;margin:0;padding:0">Lat/Long: <br /><strong>' +
+              parseInt(event.latLng.lat() * 10000,10) / 10000 + ', ' +
+              parseInt(event.latLng.lng() * 10000,10) / 10000 + '</strong></div>' +
+              '<a style="color:#1C74A5;text-decoration:none;display:block;padding:3px;" data-y="'+event.pixel.y+'" ' +
+              'data-x="'+event.pixel.x+
+              '" data-maxwidth="'+parseInt(this.$el.width() * 0.6,10)+
+              '" href="#" class="argenmapFeatureInfo">Obtener información</a>';
+      infow.setOptions({
+        position: event.latLng,
+        maxWidth: 500,
+        content: c
+      });
+      infow.open(this.gmap);
+    };
+    this.pedirInformacion = function(event) {
+      var _this = this;
+      var infow = _this.infoWindow();
+      infow.close();
+      event.preventDefault();
+      if(_this.capasWms.length === 0) {
+        alert('No hay capas para consultar');
+        return false;
+      }
+      // var maxWidth = parseInt($(event.currentTarget).data('maxwidth'),10);
+      var content = $('<div style="width:500px"  />');
+      var x = parseInt($(event.currentTarget).data('x'),10);
+      var y = parseInt($(event.currentTarget).data('y'),10);
+      var sw = _this.gmap.getBounds().getSouthWest();
+      var ne = _this.gmap.getBounds().getNorthEast();
+      var version = "1.1.1";
+      var request = "GetFeatureInfo";
+      //usando mercator para pedir 3857
+      var nw = {lat:ne.lat(),lng:sw.lng()};
+      var se = {lat:sw.lat(),lng:ne.lng()};
 
+      var crs = "EPSG:4326";
+      //minx,miny,maxx,maxy; nw son min's y se son max's
+      var bbox = sw.lng() + "," + sw.lat() + "," + ne.lng() + "," + ne.lat();
+      var service = "WMS";
+      //the size of the tile, must be 256x256
+      var width = _this.$el.width();
+      var height = _this.$el.height();
+      //Some WMS come with named styles.  The user can set to default.
+      var styles = "";
+      $.each(_this.capasWms, function(i,e){
+        if(e.consultable !== true) {
+          return;
+        }
+        var baseURL = e.url;
+        var layers = e.capas;
+        //Establish the baseURL.  Several elements, including &EXCEPTIONS=INIMAGE and &Service are unique to openLayers addresses.
+
+        var url = baseURL + 
+                  "SERVICE=" + service +
+                  "&VERSION=" + version +
+                  "&REQUEST=" + request +
+                  "&LAYERS=" + layers +
+                  "&STYLES=" + styles +
+                  "&SRS=" + crs +
+                  "&BBOX=" + bbox +
+                  "&WIDTH=" + width +
+                  "&HEIGHT=" + height +
+                  "&QUERY_LAYERS=" + layers +
+                  "&FEATURE_COUNT=50" +
+                  "&X=" + x +
+                  "&Y=" + y +
+                  "&INFO_FORMAT=text/html";
+
+        var disclaimer = '<p style="margin:0;padding:0;font-size:12px;line-height:12px;">Los datos a continuación son provistos por:<br />' +
+                        ' <strong>' + baseURL + '</strong><br />'+
+                        '<a target="_otra" href="'+url+'">Abrir en otra ventana</a></p>';
+        content.append(disclaimer);
+
+        content.append('<iframe src="'+url+'" style="margin:0;padding:0;width:480px;display:block;border:none;" />');
+        content.append('<hr />');
+      });
+      infow.setContent($('<div>').append(content).html());
+      infow.open(_this.gmap);
+      return;
+    };
     this.agregarCapaKML = function (opciones) {
       var _this = this,
         defaults = {
@@ -142,10 +251,11 @@
       var kml = new google.maps.KmlLayer(opciones);
     };
 
-    this.infoWindow = function () {
+    this.infoWindow = function (opciones) {
       if (this._infoWindow === undefined) {
         this._infoWindow = new google.maps.InfoWindow();
       }
+      this._infoWindow.setOptions($.extend({},opciones));
       return this._infoWindow;
     };
 
@@ -188,8 +298,9 @@
         if (!opciones.contenido) {
           return;
         }
-        _this.infoWindow().open(_this.$el.data('gmap'), m);
+        _this.infoWindow().close();
         _this.infoWindow().setContent(opciones.contenido);
+        _this.infoWindow().open(_this.$el.data('gmap'), m);
       });
 
       return;
@@ -339,7 +450,7 @@
      */
     this.imageMapType = null;
     /**
-     * Referencia al objeto map de google sobre el cual está capa está desplegada.
+     * Referencia al objeto map de google sobre el cual esta capa está desplegada.
      * Se setea con argenmap.agregarCapaBaseWMS().
      * @public 
      * @type google.maps.Map
@@ -371,7 +482,6 @@
       tileSize: new google.maps.Size(256, 256)
 
     };
-
 
     //Creating the object to create the ImageMapType that will call the WMS Layer Options.
 
@@ -809,6 +919,18 @@
     };
   };
 
+  // function inverseMercator(xy) {
+  //     xy.x = 180 * xy.x / 20037508.34;
+  //     xy.y = 180 / Math.PI * (2 * Math.atan(Math.exp((xy.y / 20037508.34) * Math.PI)) - Math.PI / 2);
+  //     return xy;
+  // }
+  // esta funcion (sacada de openlayers) es distinta de latLngAMercator... check
+  // function forwardMercator(xy) {
+  //     xy.x = xy.x * 20037508.34 / 180;
+  //     var y = Math.log(Math.tan((90 + xy.y) * Math.PI / 360)) / Math.PI * 20037508.34;
+  //     xy.y = Math.max(-20037508.34, Math.min(y, 20037508.34));
+  //     return xy;
+  // }
   //-----------------------------------------------------------------------//
   // jQuery event
   //-----------------------------------------------------------------------//
@@ -825,7 +947,7 @@
         if ($w !== $this.width() || $h !== $this.height()) {
           $w = $this.width();
           $h = $this.height();
-          jQuery.event.handle.call(self, {
+          jQuery.event.dispatch.call(self, {
             type: 'resized'
           });
         }
@@ -874,13 +996,21 @@
       if (!a) {
         return;
       }
-
+      var defaults = {
+        name: "Capa base WMS",
+        baseURL: "",
+        layers: "",
+        consultable: true
+      }
       var map = $(this).data('gmap');
+      var o = jQuery.extend({}, defaults, opciones);
+      a.capasWms.push(o);
+      a.activarInfoMenu();
 
       argenmap.GmapAgregarCapaBase(map, new argenmap.CapaBaseWMS({
-        name: opciones.nombre,
-        baseURL: opciones.url,
-        layers: opciones.capas
+        name: o.nombre,
+        baseURL: o.url,
+        layers: o.capas
       }));
     });
   };
@@ -911,12 +1041,21 @@
         return;
       }
 
+      var defaults = {
+        nombre: "Capa WMS",
+        url: "",
+        capas: "",
+        consultable: true
+      }
       var map = $(this).data('gmap');
+      var o = jQuery.extend({}, defaults, opciones);
+      a.capasWms.push(o);
+      a.activarInfoMenu();
 
       argenmap.GmapAgregarCapa(map, new argenmap.CapaWMS({
-        name: opciones.nombre,
-        baseURL: opciones.url,
-        layers: opciones.capas
+        name: o.nombre,
+        baseURL: o.url,
+        layers: o.capas
       }));
     });
   };
