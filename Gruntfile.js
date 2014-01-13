c9ffe596d2ea7d8c74024b2c11e26830b774ae99
osk <oskosk@gmail.com>
2014-01-13 19:38:21 -0300

    gruntfile inicial

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json');
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkgname %>.js',
+        dest: 'build/<%= pkg.name %>.min.js' 
+      }
+      
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default Task(s).
+  grunt.registerTask('default', ['uglify']);
+};
