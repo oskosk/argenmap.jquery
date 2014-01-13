34867c523ae1c1507f6f2cfad6644e98771e8ada
osk <oskosk@gmail.com>
2014-01-13 20:02:46 -0300

    corregí errores gramaticales y sintácticos en Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,15 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json');
+    pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkgname %>.js',
+        src: '<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js' 
       }
-      
+    }  
   });
 
   // Load the plugin that provides the "uglify" task.
