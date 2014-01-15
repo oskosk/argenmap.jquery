26b0d535e47aa517c635a8d67570f401267a1a8a
osk <oskosk@gmail.com>
2014-01-15 16:32:26 -0300

    mejoré el banner de gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,43 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    buildDir: 'dist',
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: [
+          '/*!',
+          ' * <%= pkg.name %> v1',
+          ' * ',
+          ' * Version     : <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>)',
+          ' * Licencia    : <%= pkg.license.url %>',
+          ' * Web site    : http://ign.gob.ar/argenmap',          
+          ' * Repositorio : http://github.com/oskosk/argenmap.jquery',
+          ' * Issues      : https://github.com/oskosk/argenmap.jquery/issues',
+          ' * Contacto    : argenmap@ign.gob.ar',
+          ' * ',
+          ' * Instituto Geografico Nacional de Argentina',
+          ' * Todos los derechos reservados.',
+          ' *',
+          ' */\n\n'
+        ].join('\n'),        
       },
       build: {
-        src: '<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js' 
+        options: {
+          'preserveComments':'some'
+        },
+        src: 'src/<%= pkg.name %>.js',
+        dest: '<%= buildDir %>/<%= pkg.name %>.min.js' 
+      },
+      build2: {
+        options: {
+          'mangle': false,
+          'preserveComments': false,
+	        'compress': false,
+          'beautify': true
+        },
+        files: {
+          '<%= buildDir %>/<%= pkg.name %>.js': [ 'src/<%= pkg.name %>.js'],
+        }
       }
     }  
   });
