20aec9ee9fdc4ea5d798c98b7d9f28bedf593e95
osk <oskosk@gmail.com>
2014-01-27 18:05:17 -0300

    eliminé el uso de indexOf. Lo reemplacé por el uso de $.inArray

--- src/argenmap.jquery.js
+++ src/argenmap.jquery.js
@@ -372,27 +372,6 @@
     }
 
   };
-  /* COMPATIBILIDAD CON IE < 9; implementacion de indexOf para arrays */
-  if (!Array.prototype.indexOf)
-  {
-    Array.prototype.indexOf = function(elt /*, from*/)
-    {
-      var len = this.length >>> 0;
-
-      var from = Number(arguments[1]) || 0;
-      from = (from < 0) ? Math.ceil(from) : Math.floor(from);
-      if (from < 0)
-        from += len;
-
-      for (; from < len; from++)
-      {
-        if (from in this &&
-            this[from] === elt)
-          return from;
-      }
-      return -1;
-    };
-  }
 
   /**
    * Clase de cache interna de urls
@@ -415,7 +394,7 @@
     {
       var tilecode = x + '-' + y + '-' + z;
 
-      if(this.cache.indexOf(tilecode) !== -1) 
+      if( $.inArray(tilecode, this.cache) !== -1) 
       {
         return this.cacheRef[tilecode];
       }
